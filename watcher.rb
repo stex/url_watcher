b85141ea4c0a70146d7bfcad31cadbe26bc5b1d5
Stefan Exner <stex@sterex.de>
2021-05-16 23:18:53 +0200

    better watcher script

--- watcher.rb
+++ watcher.rb
@@ -1,14 +1,27 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "net/http"
-require "fileutils"
+require 'net/http'
+require 'fileutils'
 
-URL = URI(ENV.fetch("PAGE_URL"))
-WEBHOOK_URL = URI(ENV.fetch("WEBHOOK_URL"))
-CHECK_INTERVAL = ENV.fetch("CHECK_INTERVAL", 60).to_i
+URL = URI(ENV.fetch('PAGE_URL'))
+WEBHOOK_URL = URI(ENV.fetch('WEBHOOK_URL'))
+CHECK_INTERVAL = ENV.fetch('CHECK_INTERVAL', 60).to_i
 
-OLD_FILE = File.expand_path("old.html", __dir__)
-CURRENT_FILE = File.expand_path("current.html", __dir__)
+OLD_FILE = File.expand_path('old.html', __dir__)
+CURRENT_FILE = File.expand_path('current.html', __dir__)
+
+check_string = "Checking #{URL} every #{CHECK_INTERVAL} seconds..."
+
+puts <<~GREETING
+  **#{'*' * check_string.length}**
+  * #{check_string} *
+  **#{'*' * check_string.length}**
+GREETING
+
+def log(msg)
+  puts "[#{Time.now}] #{msg}"
+end
 
 loop do
   content = Net::HTTP.get(URL)
@@ -18,11 +31,9 @@ loop do
   if File.exist?(OLD_FILE)
     diff_output = `diff #{OLD_FILE} #{CURRENT_FILE}`
 
-    if diff_output.length.zero?
-      puts "Not Changed"
-    else
-      puts "Content changed, triggering webhook."
-      Net::HTTP.post(WEBHOOK_URL, "")
+    unless diff_output.length.zero?
+      log 'Content changed, triggering webhook'
+      Net::HTTP.post(WEBHOOK_URL, '')
     end
 
     FileUtils.rm(OLD_FILE)
